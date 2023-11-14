390c33c5c84b11d9ea6f5d12bf79f9c16ad00df9
mrrobjn <votruongdung4@gmail.com>
2023-11-14 14:49:15 +0700

    be login and logout

--- server/app/Http/Controllers/AuthController.php
+++ server/app/Http/Controllers/AuthController.php
@@ -6,19 +6,22 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Auth;
 use Illuminate\Support\Facades\Validator;
+
 class AuthController extends Controller
 {
-    public function __construct(){
-        $this->middleware("auth:api",['except'=>['login','register']]); // Middleware "auth:api" is used to authenticate users via API token
+    public function __construct()
+    {
+        $this->middleware("auth:api", ['except' => ['login', 'register']]); // Middleware "auth:api" is used to authenticate users via API token
     }
-    public function register(Request $request){
-        $validator = Validator::make($request->all(),[
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
             'name' => 'required',
             'email' => 'required|email|unique:users',
             'password' => 'required|string|confirmed|min:6',
         ]);
-        if($validator->fails()){
-            return response()->json($validator->errors(),422);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
         }
 
         $user = User::create(array_merge(
@@ -27,10 +30,40 @@ class AuthController extends Controller
         ));
 
         return response()->json([
-            'message' =>'User successfully registered',
-            'user' =>$user,
-        ],201);
+            'message' => 'User successfully registered',
+            'user' => $user,
+        ], 201);
     }
-    public function login(Request $request){
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        if (!$token = auth()->attempt($validator->validated())) {
+            return response()->json("Unauthorized", 401);
+        }
+        return $this->createNewToken($token);
+    }
+    public function createNewToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => config('jwt.ttl') * 60,
+            'user' => auth()->user()
+        ]);
+    }
+    public function profile()
+    {
+        return response()->json(auth()->user());
+    }
+    public function logout()
+    {
+        auth()->logout();
+        return response()->json(['message' => 'User logged out']);
     }
 }
