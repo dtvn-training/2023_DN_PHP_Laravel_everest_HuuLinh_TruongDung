2227986b6175fd0389da53360974446216720b7c
HuuLinh25 <huulinh250702@gmail.com>
2023-12-06 21:19:45 +0700

    upload image to cloudinary

--- server/app/Http/Controllers/CampaignController.php
+++ server/app/Http/Controllers/CampaignController.php
@@ -7,7 +7,7 @@ use App\Models\Creative;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
-
+use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 use Illuminate\Http\Request;
 
 class CampaignController extends Controller
@@ -30,14 +30,13 @@ class CampaignController extends Controller
         $currentUser = Auth::user();
         $validator = Validator::make($request->all(), [
             'campaign_name' => 'required',
-            'status' => 'required|in:1,0', //1:active 0: inactive
+            'status' => 'required|in:1,0',
             'budget' => 'required|numeric|min:0',
             'bid_amount' => 'required|numeric|min:0',
             'start_date' => 'required',
             'end_date' => 'required',
             'creatives.*.creative_name' => 'required',
             'creatives.*.final_url' => 'required',
-            'creatives.*.preview_image' => 'required',
             'creatives.*.description' => 'required',
         ]);
 
@@ -61,19 +60,27 @@ class CampaignController extends Controller
         if (!$campaign) {
             return response()->json(['message' => "Campaign not found"], 404);
         }
-
         $campaign->update($campaignData);
-        // Update Creatives
-        $creativesData = $request->only([
-            'creative_name',
-            'final_url',
-            'preview_image',
-            'description',
-        ]);
 
-        // Lặp qua tất cả các creative và cập nhật dữ liệu
-        foreach ($campaign->creatives as $creative) {
-            $creative->update($creativesData);
+        // Update or Create Creatives
+        if ($request->has('creatives') && is_array($request->creatives)) {
+            foreach ($request->creatives as $creativeData) {
+                $creative = Creative::updateOrCreate(
+                    ['id' => $creativeData['id'] ?? null],
+                    [
+                        'creative_name' => $creativeData['creative_name'],
+                        'final_url' => $creativeData['final_url'],
+                        'description' => $creativeData['description'],
+                    ]
+                );
+
+                // If a new preview_image is provided, upload it to Cloudinary and update the URL
+                if (isset($creativeData['preview_image'])) {
+                    $uploadedFileUrl = Cloudinary::upload($request->file('preview_image')->getRealPath())->getSecurePath();
+                    $creative->preview_image = $uploadedFileUrl;
+                    $creative->save();
+                }
+            }
         }
 
         return response()->json(['message' => 'Update campaign and creatives successfully']);
@@ -124,7 +131,8 @@ class CampaignController extends Controller
             'description',
         ]);
         $creativeData['id_campaign'] = $newCampaign->id;
-        
+        $uploadedFileUrl = Cloudinary::upload($request->file('preview_image')->getRealPath())->getSecurePath();
+        $creativeData['preview_image'] = $uploadedFileUrl;
         $newCreative = Creative::create($creativeData);
         $newCreative->save();
 
@@ -134,8 +142,10 @@ class CampaignController extends Controller
     {
         $campaigns = Campaign::with('creatives')->paginate(3);
 
-        return response()->json(
-            $campaigns
-        );
+        if ($campaigns->isEmpty()) {
+            return response()->json(['message' => 'There are no campaigns!']);
+        }
+
+        return response()->json($campaigns);
     }
 }
