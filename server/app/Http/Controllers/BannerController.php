1f93f7df0fd19a24e668ca62741de99ef0309f32
Huulinh25 <huulinh250702@gmail.com>
2023-12-14 09:38:26 +0700

    update status 1.1

--- server/app/Http/Controllers/BannerController.php
+++ server/app/Http/Controllers/BannerController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Campaign;
+use App\Models\Creative;
 use Carbon\Carbon;
-
 class BannerController extends Controller
 {
     public function impression($id)
@@ -18,23 +18,25 @@ class BannerController extends Controller
 
         $bid_amount = $campaign->bid_amount;
         $used_amount = $campaign->used_amount + $bid_amount;
+        $usage_rate = ($used_amount / $campaign->budget) * 100;
+        $remain = $campaign->budget - $used_amount;
         $endDate = $campaign->end_date;
         $currentDateTime = Carbon::now();
-        $remain = $campaign->budget - $used_amount;
-
         // Check end conditions
-        if ($endDate <= $currentDateTime && $remain <  $campaign->bid_amount) {
+        if($endDate <= $currentDateTime) {
+            $campaign->status = 0;
+            return response()->json(['message' => 'Please choose end date after current date'],422);
+        }
+        if ($remain <  $campaign->bid_amount) {
             $campaign->status = 0;
             $campaign->save();
-            return response()->json(['message' => 'Hết tiền', 'status' => $campaign->status]);
-        } else {
+            return response()->json(['message' => 'Hết tiền', 422]);
+        }else {
             // Save the changes to the database
             $campaign->used_amount = $used_amount;
+            $campaign->usage_rate = $usage_rate;
             $campaign->save();
-
-            $campaigns = Campaign::with('creatives')->where('status', 1)->paginate(3);
-
-            return response()->json($campaigns);
+            return response()->json(['message' => $campaign]);
         }
     }
 
@@ -45,3 +47,51 @@ class BannerController extends Controller
         return response()->json($campaigns);
     }
 }
+
+// <?php
+
+// namespace App\Http\Controllers;
+
+// use Illuminate\Http\Request;
+// use App\Models\Campaign;
+// use Carbon\Carbon;
+
+// class BannerController extends Controller
+// {
+//     public function impression($id)
+//     {
+//         $campaign = Campaign::with('creatives')->find($id);
+
+//         if (!$campaign) {
+//             return response()->json(['message' => "Campaign not found"], 404);
+//         }
+
+//         $bid_amount = $campaign->bid_amount;
+//         $used_amount = $campaign->used_amount + $bid_amount;
+//         $endDate = $campaign->end_date;
+//         $currentDateTime = Carbon::now();
+//         $remain = $campaign->budget - $used_amount;
+
+//         // Check end conditions
+//         if ($endDate <= $currentDateTime && $remain <  $campaign->bid_amount) {
+//             $campaign->status = 0;
+//             $campaign->save();
+//             return response()->json(['message' => 'Hết tiền', 'status' => $campaign->status]);
+//         } else {
+//             // Save the changes to the database
+//             $campaign->used_amount = $used_amount;
+//             $campaign->save();
+
+//             $campaigns = Campaign::with('creatives')->where('status', 1)->paginate(3);
+
+//             return response()->json($campaigns);
+//         }
+//     }
+
+//     public function getBanner()
+//     {
+//         $campaigns = Campaign::with('creatives')->where('status', 1)->paginate(3);
+
+//         return response()->json($campaigns);
+//     }
+// }
